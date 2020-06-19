50ab050f8bee169105485b9967ed7532039ae40f
shwetajoshi2896 <48716740+shwetajoshi2896@users.noreply.github.com>
2020-06-19 13:04:40 +0530

    Updates

---
+++ precache-manifest.0cd0c3298125ca035dda171277a7012b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0b4e1771268327cc9a5b349147b597b",
+    "url": "/coalbrass-fashions/index.html"
+  },
+  {
+    "revision": "83e0ddad7df38bacf423",
+    "url": "/coalbrass-fashions/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db7a627cbc2b935499d5",
+    "url": "/coalbrass-fashions/static/js/2.a925df73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coalbrass-fashions/static/js/2.a925df73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e0ddad7df38bacf423",
+    "url": "/coalbrass-fashions/static/js/main.b18364c9.chunk.js"
+  },
+  {
+    "revision": "159f7113dcf628f628a9",
+    "url": "/coalbrass-fashions/static/js/runtime-main.f45e60b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coalbrass-fashions/static/media/logo.5d5d9eef.svg"
+  }
+]);
