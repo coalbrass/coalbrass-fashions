ecba90647b1611aa33341a38563fd0b20cc12a03
shwetajoshi2896 <48716740+shwetajoshi2896@users.noreply.github.com>
2020-06-19 13:23:50 +0530

    added 3 new routes on app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Beta from "./components/Beta";
 import Landing from "./components/LandingPage";
+import Rebelle from "./components/Rebelle";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Landing />
+      <Router>
+        <Route path="/" component={Rebelle} exact />
+        <Route path="/beta2" component={Landing} />
+        <Route path="/beta3" component={Beta} />
+      </Router>
     </div>
   );
 }
